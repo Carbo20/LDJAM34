dbcb3206abd9da232c0170d3bd2ccb1c4b89a888
carbo20 <carbo20@hotmail.fr>
2015-12-12 18:30:11 +0100

    add projectile generator

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -5,16 +5,100 @@ public class Obstacles : MonoBehaviour {
     [SerializeField] float m_RotationSpeed = 5f;
     [SerializeField] float m_MoveX = 0.001f;
     [SerializeField] float m_MoveY = 0.001f;
+    [SerializeField] float lifeTime;
     Transform myTransform;
 
-	// Use this for initialization
-	void Start () {
+    bool isProjectile = false;
+    float deltaTime;
+
+   
+    // Use this for initialization
+    void Start () {
         myTransform = this.transform;
+        deltaTime = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.forward * m_RotationSpeed);
-        transform.Translate(new Vector3(m_MoveX, m_MoveY));
+        transform.Rotate(Vector3.forward * RotationSpeed);
+        transform.Translate(new Vector3(MoveX, MoveY));
+        if (IsProjectile)
+        {
+
+            Debug.Log("COUCOU");
+            deltaTime += Time.deltaTime;
+            if(deltaTime > LifeTime)
+            {
+                Debug.Log("COUCOU");
+                Destroy(gameObject);
+            }
+        }
 	}
+
+
+
+    public float RotationSpeed
+    {
+        get
+        {
+            return m_RotationSpeed;
+        }
+
+        set
+        {
+            m_RotationSpeed = value;
+        }
+    }
+
+    public float MoveX
+    {
+        get
+        {
+            return m_MoveX;
+        }
+
+        set
+        {
+            m_MoveX = value;
+        }
+    }
+
+    public float MoveY
+    {
+        get
+        {
+            return m_MoveY;
+        }
+
+        set
+        {
+            m_MoveY = value;
+        }
+    }
+
+    public float LifeTime
+    {
+        get
+        {
+            return lifeTime;
+        }
+
+        set
+        {
+            lifeTime = value;
+        }
+    }
+
+    public bool IsProjectile
+    {
+        get
+        {
+            return isProjectile;
+        }
+
+        set
+        {
+            isProjectile = value;
+        }
+    }
 }
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private float timeBetweenTwoProjectile;
+    [SerializeField]
+    private float speedX, speedY;
+    [SerializeField]
+    private float lifeTime;
+
+    [SerializeField]
+    private GameObject projectile;
+
+    private float deltaTime;
+
+	// Use this for initialization
+	void Start () {
+        deltaTime = 0f;
+        projectile.GetComponent<Obstacles>().MoveX = speedX;
+        projectile.GetComponent<Obstacles>().MoveY = speedY;
+        projectile.GetComponent<Obstacles>().LifeTime = lifeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GenerateProjectile();
+	}
+
+    private void GenerateProjectile()
+    {
+        if(deltaTime > timeBetweenTwoProjectile)
+        {
+            GameObject instance = Instantiate(projectile);
+            instance.GetComponent<Obstacles>().IsProjectile = true;
+            deltaTime = 0f;
+        }
+        deltaTime += Time.deltaTime;
+    }
+
+}

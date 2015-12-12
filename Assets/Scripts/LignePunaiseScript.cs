d0b6c112e1f92f15e01df3e6ec7fdffcc0b2cb57
David YANA <david.yana@yahoo.fr>
2015-12-12 23:59:14 +0100

    ligne de punaise

---
+++ Assets/Scripts/LignePunaiseScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LignePunaiseScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform start, end;
+    [SerializeField]
+    private int nbPin;
+    [SerializeField]
+    private GameObject pin;
+    
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < nbPin; i++)
+        {
+            GameObject gao = Instantiate(pin);
+            gao.transform.position = Vector2.Lerp(start.position, end.position, (float)i /  (float)nbPin);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(start.position, end.position);
+    }
+}

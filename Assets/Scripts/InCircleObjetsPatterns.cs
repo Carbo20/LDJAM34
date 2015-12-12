44998977618751ae0f4c5a7f4f6fb3d49971de62
Cricroo <crizz13@hotmail.fr>
2015-12-12 22:45:40 +0100

    And the perfect circle was born

---
+++ Assets/Scripts/InCircleObjetsPatterns.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InCircleObjetsPatterns : MonoBehaviour
+{
+
+    // Instantiates a prefab in a circle
+
+    public GameObject prefab;
+
+    public int numberOfObjects = 20;
+    public float radius = 5f;
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+            GameObject go = Instantiate(prefab) as GameObject;
+            go.transform.position = pos;
+            
+                        Vector2 dir = (Vector3.zero-go.transform.position).normalized;
+                        //transform.position = transform.position;
+
+                        float angleBis = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+                        go.transform.rotation = Quaternion.AngleAxis(angleBis, Vector3.forward);
+            
+        }
+    }
+}

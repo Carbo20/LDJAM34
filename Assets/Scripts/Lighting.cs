3053aca24eab3dfe8dca0552f42d61b180ceea7d
carbo20 <carbo20@hotmail.fr>
2015-12-13 20:54:14 +0100

    tesla animation

--- Assets/Scripts/Lighting.cs
+++ Assets/Scripts/Lighting.cs
@@ -7,19 +7,33 @@ public class Lighting : MonoBehaviour {
     [SerializeField] private float m_TicDuration = 2f;
     [SerializeField] private bool m_ModifyX = false;
     [SerializeField] private bool m_ModifyY = false;
+    [SerializeField]
+    private Sprite energie0, energie1, energie2, energie3;
+    [SerializeField]
+    private GameObject tesla1, tesla2;
+
     private Transform myTransform;
     public bool lethal = true;
     [SerializeField]
     private float ticActualDuration;
     private float timeBetweenAnimation;
+    private float ticEnergieChange;
     private SpriteRenderer mySpriteRenderer;
+
+
+    private float timeChangeEnergie;
+
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
         myTransform = this.transform;
         ticActualDuration = 0f;
         timeBetweenAnimation = 0f;
-        mySpriteRenderer = GetComponent<SpriteRenderer>(); 
+        ticEnergieChange = 0f;
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        timeChangeEnergie =( m_TicDuration)/ 4;
+        animator = tesla1.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -31,6 +45,7 @@ public class Lighting : MonoBehaviour {
             lethal = false;
             gameObject.tag = "Untagged";
             mySpriteRenderer.enabled = false;
+            
 
         }
         else if (ticActualDuration <= m_TicDuration) // enabled and animated
@@ -54,5 +69,35 @@ public class Lighting : MonoBehaviour {
         }
         else
             ticActualDuration = 0f;
-	}
+
+        ticEnergieChange += Time.deltaTime;
+        if (ticEnergieChange >= timeChangeEnergie)
+        {
+            ticEnergieChange = 0f;
+            AnimTesla(tesla1);
+            AnimTesla(tesla2);
+        }
+    }
+
+    private void AnimTesla(GameObject tesla)
+    {
+        tesla.GetComponent<Animator>().SetTrigger("energieUp");
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Empty"))
+        {
+            tesla.GetComponent<SpriteRenderer>().sprite = energie0;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Energie1"))
+        {
+            tesla.GetComponent<SpriteRenderer>().sprite = energie1;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Energie2"))
+        {
+            tesla.GetComponent<SpriteRenderer>().sprite = energie2;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Energie3"))
+        {
+            tesla.GetComponent<SpriteRenderer>().sprite = energie3;
+        }
+    }
+
 }

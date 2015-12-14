25c54e4f59cdac668ff914ddec0df564af2709fc
David YANA <david.yana@yahoo.fr>
2015-12-14 19:29:00 +0100

    HUB multicolor *_*

--- Assets/Scripts/InCircleObjetsPatterns.cs
+++ Assets/Scripts/InCircleObjetsPatterns.cs
@@ -10,6 +10,8 @@ public class InCircleObjetsPatterns : MonoBehaviour
 
     public int numberOfObjects = 20;
     public float radius = 5f;
+    [SerializeField]
+    private bool randomColor;
 
     void Start()
     {
@@ -18,14 +20,16 @@ public class InCircleObjetsPatterns : MonoBehaviour
             float angle = i * Mathf.PI * 2 / numberOfObjects;
             Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
             GameObject go = Instantiate(prefab) as GameObject;
+            if (randomColor)
+                go.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             go.transform.position = pos;
             
-                        Vector2 dir = (Vector3.zero-go.transform.position).normalized;
-                        //transform.position = transform.position;
+            Vector2 dir = (Vector3.zero-go.transform.position).normalized;
+            //transform.position = transform.position;
 
-                        float angleBis = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            float angleBis = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-                        go.transform.rotation = Quaternion.AngleAxis(angleBis, Vector3.forward);
+            go.transform.rotation = Quaternion.AngleAxis(angleBis, Vector3.forward);
             
         }
     }
